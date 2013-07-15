99dc62fd89424047813bda5a959f43cd252c514f
Thiago Rondon <thiago.rondon@gmail.com>
2013-07-15 20:58:03 -0300

    first commit for install.sh
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CURL=/usr/bin/curl
+
+if [ "$(id -u)" == "0" ]; then
+   echo "This script can be run as root" 1>&2
+   exit 1
+fi
+
+if which curl >/dev/null; then
+    CURL=`which curl`
+else
+    echo "please, install curl." 1>&2
+    exit
+fi
+
+$CURL -kL http://install.perlbrew.pl | bash
+source ~/perl5/perlbrew/etc/bashrc
+echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bashrc
+perlbrew install -n perl-5.12.2
+perlbrew switch perl-5.12.2
+perlbrewl install-cpanm
+perlbrew install-cpanm
+cpanm inc::Module::Install
+cpanm local::lib Log::Syslog::Fast MooseX::Traits
+cpanm Module::Extract::Use
+“perl -MModule::Extract::Use -E 'say $_ for Module::Extract::Use->new->get_modules(@ARGV)' backup_agent| cpanm”
+cpanm LWP::Protocol::https
+mkdir $USER/bin
+$CURL -X GET https://raw.github.com/b-datum/b-datum-backup-agent-linux-perl/master/backup_agent > $USER/bin/backup_agent
+
+
