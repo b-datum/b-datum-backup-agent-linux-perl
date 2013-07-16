33f15bf7ebcfe2035df542d5f0b73a3d657b6330
Thiago Rondon <thiago.rondon@gmail.com>
2013-07-15 22:34:36 -0300

    cpanm -n for Module::Extract::Use->new->get_modules
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ cpanm -n inc::Module::Install local::lib Log::Syslog::Fast MooseX::Traits Module
 mkdir -p $HOME/bin
 $CURL -X GET https://raw.github.com/b-datum/b-datum-backup-agent-linux-perl/master/backup_agent > $HOME/bin/backup_agent
 
-perl -MModule::Extract::Use -E 'say $_ for Module::Extract::Use->new->get_modules(@ARGV)' $HOME/bin/backup_agent| cpanm
+perl -MModule::Extract::Use -E 'say $_ for Module::Extract::Use->new->get_modules(@ARGV)' $HOME/bin/backup_agent| cpanm -n
 cpanm LWP::Protocol::https
 
 chmod +x $HOME/bin/backup_agent
