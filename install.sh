264e872c44b82d0e3447f1699ad4de84f5a4ab0d
Thiago Rondon <thiago.rondon@gmail.com>
2013-07-15 22:15:24 -0300

    use $HOME
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- install.sh
+++ install.sh
@@ -22,8 +22,8 @@ perlbrew install -n perl-5.12.2
 perlbrew switch perl-5.12.2
 perlbrew install-cpanm
 cpanm -n inc::Module::Install local::lib Log::Syslog::Fast MooseX::Traits Module::Extract::Use
-“perl -MModule::Extract::Use -E 'say $_ for Module::Extract::Use->new->get_modules(@ARGV)' backup_agent| cpanm”
+perl -MModule::Extract::Use -E 'say $_ for Module::Extract::Use->new->get_modules(@ARGV)' backup_agent| cpanm
 cpanm LWP::Protocol::https
-mkdir $USER/bin
-$CURL -X GET https://raw.github.com/b-datum/b-datum-backup-agent-linux-perl/master/backup_agent > $USER/bin/backup_agent
+mkdir $HOME/bin
+$CURL -X GET https://raw.github.com/b-datum/b-datum-backup-agent-linux-perl/master/backup_agent > $HOME/bin/backup_agent
 
