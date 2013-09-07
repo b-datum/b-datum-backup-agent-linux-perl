7acf70f6d3dcd50223fd300c462af71b92e70919
Thiago Rondon <thiago.rondon@gmail.com>
2013-09-07 12:22:01 -0300

    shebang
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- scripts/fatpack.sh
+++ scripts/fatpack.sh
@@ -1,6 +1,6 @@
 
-( echo "#/usr/bin/env perl" ; fatpack file ; cat bdatum-backup ) > bin/bdatum-backup
-( echo "#/usr/bin/env perl" ; fatpack file ; cat bdatum-restore ) > bin/bdatum-restore
+( echo "#!/usr/bin/env perl" ; fatpack file ; cat bdatum-backup ) > bin/bdatum-backup
+( echo "#!/usr/bin/env perl" ; fatpack file ; cat bdatum-restore ) > bin/bdatum-restore
 
 chmod 755 bin/bdatum-backup
 chmod 755 bin/bdatum-restore
