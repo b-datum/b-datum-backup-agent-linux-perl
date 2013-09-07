8248b5a456285cf868112369195df99d39c5c5cb
Thiago Rondon <thiago.rondon@gmail.com>
2013-09-07 12:09:15 -0300

    fix fatpack
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- scripts/fatpack.sh
+++ scripts/fatpack.sh
@@ -1,6 +1,6 @@
 
-( fatpack file ; cat bdatum-backup ) > bin/bdatum-backup
-( fatpack file ; cat bdatum-restore ) > bin/bdatum-restore
+( echo "#/usr/bin/env perl" ; fatpack file ; cat bdatum-backup ) > bin/bdatum-backup
+( echo "#/usr/bin/env perl" ; fatpack file ; cat bdatum-restore ) > bin/bdatum-restore
 
 chmod 755 bin/bdatum-backup
 chmod 755 bin/bdatum-restore
