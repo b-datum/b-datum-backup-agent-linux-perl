f28eb4f8d17eee184c5a285cdbc189faecae6168
Thiago Rondon <thiago.rondon@gmail.com>
2013-09-13 23:34:19 -0300

    add debian/watch
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- scripts/debpackage.sh
+++ scripts/debpackage.sh
@@ -21,8 +21,15 @@ rm -rf $TARGET_DIR/fatlib
 DEB_BUILD_OPTIONS="nocheck" \
 DEBFULLNAME="Thiago Rondon" \
 EMAIL="staff@b-datum.com" \
-dh-make-perl $TARGET_DIR --desc 'b-datum backup' --email 'staff@b-datum.com' --packagename 'b-datum-backup-perl' --intrusive --depends dialog
-cp $TARGET_DIR/linux/postinst $TARGET_DIR/debian/postinst
+
+dh-make-perl $TARGET_DIR \
+	--desc 'Provides tools for backup and restore files with b-datum.com' \
+	--email 'staff@b-datum.com' \
+	--packagename 'b-datum' \
+	--intrusive --depends dialog
+
+cp $TARGET_DIR/linux/debian/postinst $TARGET_DIR/debian/postinst
+cp $TARGET_DIR/linux/debian/watch $TARGET_DIR/debian/watch
 cp $TARGET_DIR/Changes $TARGET_DIR/debian/changelog
 (cd $TARGET_DIR && debuild --no-tgz-check -us -uc)
 
