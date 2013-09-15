b443a877e290b18d4ffd52f11498b0677d29c98f
Thiago Rondon <thiago.rondon@gmail.com>
2013-09-14 23:35:15 -0300

    add trace of fatpack to repo
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- scripts/fatpack.sh
+++ scripts/fatpack.sh
@@ -1,6 +1,10 @@
 
 for file in bdatum-backup bdatum-restore ; do
 
+#	fatpack trace src/$file
+#	fatpack packlists-for `cat fatpacker.trace` > packlists
+#	fatpack tree `cat packlists`
+
 	( cat src/header.pl ; \
 		fatpack file ; \
 		cat src/$file ) \
