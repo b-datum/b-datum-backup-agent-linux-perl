cedc96745904b3fff033a6f0df132127d843b363
Thiago Rondon <thiago.rondon@gmail.com>
2014-01-22 17:14:50 -0200

    update for 1.03
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- install-mac.sh
+++ install-mac.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 LABEL="com.bdatum.backup.mac.plist"
-PLIST="/Library/LaunchDaemons/$LABEL"
 PLISTU="$HOME/Library/LaunchAgents/$LABEL"
-
-# retrocompatible
-if [ -f $PLIST ] ; then
-  launchctl unload -w $PLIST
-fi
 
 if [ -f $PLISTU ] ; then
   launchctl unload -w $PLISTU
@@ -18,11 +12,6 @@ sudo cp bin/bdatum-restore /usr/bin
 sudo cp bin/bdatum-config /usr/bin
 
 bdatum-config
-
-# retrocompatible
-if [ -f $PLIST ] ; then
-  sudo rm $PLIST
-fi
 
 if [ -f $PLISTU ] ; then
   sudo rm $PLISTU
