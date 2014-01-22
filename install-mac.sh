cf6ec89ee136f54a9d30bae6fcae356702d0c679
Thiago Rondon <thiago.rondon@gmail.com>
2014-01-22 12:19:05 -0200

    per-user configuration files (LaunchAgents) for launchctl
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- install-mac.sh
+++ install-mac.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-LABEL="com.bdatum.backup.mac"
-PLIST="/Library/LaunchDaemons/$LABEL.plist"
+LABEL="com.bdatum.backup.mac.plist"
+PLIST="/Library/LaunchDaemons/$LABEL"
+PLISTU="$HOME/Library/LaunchAgents/$LABEL"
 
+# retrocompatible
 if [ -f $PLIST ] ; then
   launchctl unload -w $PLIST
 fi
 
-sudo cp bin/bdatum-backup /usr/bin
+if [ -f $PLISTU ] ; then
+  launchctl unload -w $PLISTU
+fi
+
+sudo -p "Please, enter root password:" cp bin/bdatum-backup /usr/bin
 sudo cp bin/bdatum-restore /usr/bin
 sudo cp bin/bdatum-config /usr/bin
 
 bdatum-config
 
-sudo cp macos/$LABEL $PLIST
-launchctl load -w $PLIST
+# retrocompatible
+if [ -f $PLIST ] ; then
+  sudo rm $PLIST
+fi
+
+if [ -f $PLISTU ] ; then
+  sudo rm $PLISTU
+fi
+
+cp macos/$LABEL $PLISTU
+
+launchctl load -w $PLISTU
 
