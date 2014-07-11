9dca07bfd9997f964afc7a357751943a9455f7ed
Thiago Rondon <thiago.rondon@gmail.com>
2014-07-11 12:03:57 -0300

    exporter tags
    Signed-off-by: Thiago Rondon <thiago.rondon@gmail.com>

--- lib/bdatum/Constants.pm
+++ lib/bdatum/Constants.pm
@@ -24,21 +24,21 @@ use constant SYMLINK => 5;
 # 3xx - Local Error
 # 4xx - Setup Error
 
-use constant ONLINE => 100;              # I'm here
-use constant BACKUP => 101;              # Doing..
+use constant ONLINE => 100;    # I'm here
+use constant BACKUP => 101;    # Doing..
 
-use constant OK_ALL => 200;              # Withour problem
+use constant OK_ALL           => 200;    # Withour problem
 use constant OK_UPDATE_CONFIG => 201;    # Configuração atualizada
 
-use constant ERR_PERM_ETC => 301;        # Please run 'chmod 0400 /configfile'
-use constant ERR_NO_PATH => 302;         # No path information
-use constant ERR_PATH_PERM => 303;       # Path without permission to backup
-use constant ERR_FILE_PERM => 304;       # No permission to a file
+use constant ERR_PERM_ETC   => 301;      # Please run 'chmod 0400 /configfile'
+use constant ERR_NO_PATH    => 302;      # No path information
+use constant ERR_PATH_PERM  => 303;      # Path without permission to backup
+use constant ERR_FILE_PERM  => 304;      # No permission to a file
 use constant ERR_NO_PERL_S3 => 340;      # need to install Net::Amazon::S3
 
-use constant ERR_PART_SIZE => 401;       # part_size option need to be > 5 or < 500
-use constant ERR_PATH => 402;            # No path to backup
-use constant ERR_TOO_MANY_TRY => 403;    # Too many times try to send something.
+use constant ERR_PART_SIZE    => 401; # part_size option need to be > 5 or < 500
+use constant ERR_PATH         => 402; # No path to backup
+use constant ERR_TOO_MANY_TRY => 403; # Too many times try to send something.
 
 require Exporter;
 
@@ -48,6 +48,10 @@ our %EXPORT_TAGS = (
     qw/
       BASE_URL BASE_DIR LIMIT_PARTS
       MTIME ETAG UID GID UMASK SYMLINK
+      ONLINE BACKUP OK_ALL OK_UPDATE_CONFIG
+      ERR_PERM_ETC ERR_NO_PATH ERR_PATH_PERM ERR_FILE_PERM ERR_NO_PERL_S3
+      ERR_PART_SIZE ERR_PATH ERR_TOO_MANY_TRY
+
       /
   ]
 );
